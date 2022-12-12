7969fddb3af02442aaf4bd3761d8fe62b64827d2
GunHo <noh3310@gmail.com>
2022-12-12 18:41:14 +0900

    first commit

---
+++ MedicineTime/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MedicineTime
+//
+//  Created by 노건호 on 2022/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MedicineTime/MedicineTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicineTimeApp.swift
+//  MedicineTime
+//
+//  Created by 노건호 on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct MedicineTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
