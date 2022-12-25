70b5a6dfc2911885d1ce378bcf86717d95143075
GH <noh3310@gmail.com>
2022-12-26 02:44:50 +0900

    #3 - APIKey 파일 분리

---
+++ MedicineTime/API/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  MedicineTime
+//
+//  Created by 노건호 on 2022/12/26.
+//
+
+import Foundation
+
+class APIKeys {
+    static let serviceKey = "cMiqI4R%2FQ5rATDh2y7A0SpXL4a0GE%2BxOEaw3udrFUaeZGFfznftuq0a19G07PcoiRtBxehUxI9gV9jDim97bUw%3D%3D"
+}
--- MedicineTime/API/BaseAPI.swift
+++ MedicineTime/API/BaseAPI.swift
@@ -14,12 +14,10 @@ class BaseAPI {
     
     static let shared = BaseAPI()
     
-    let serviceKey = "cMiqI4R%2FQ5rATDh2y7A0SpXL4a0GE%2BxOEaw3udrFUaeZGFfznftuq0a19G07PcoiRtBxehUxI9gV9jDim97bUw%3D%3D"
-    
     func getApi(_ name: String) -> AnyPublisher<[Item], Error> {
 
         let encodeName = name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-        let urlStr = "https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=cMiqI4R%2FQ5rATDh2y7A0SpXL4a0GE%2BxOEaw3udrFUaeZGFfznftuq0a19G07PcoiRtBxehUxI9gV9jDim97bUw%3D%3D&itemName=\(encodeName)&type=json"
+        let urlStr = "https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=\(APIKeys.serviceKey)&itemName=\(encodeName)&type=json"
         let url = URL(string: urlStr)!
         
         return URLSession.shared.dataTaskPublisher(for: url)
